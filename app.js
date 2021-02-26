5dc29ff70db409c0128c3d788ca458e783c0ac4a
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-02-26 14:42:13 +0530

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>")
+});
+
+module.exports = app;
+//app.listen(3001);
